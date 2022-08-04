5f9d15a9b75bd9cd0ba9a0bde5c5bdbe55e25827
Leticia Baldin Galvani <leticiagalvani2003@gmail.com>
2022-08-04 11:14:38 -0300

    :sparkles: Init project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ConverterApp());
+}
+
+class ConverterApp extends StatelessWidget {
+  const ConverterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
